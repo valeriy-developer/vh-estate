abb679d8874101b8f6f74d4312b144d1fe1e8eba
valeriy-developer <valeriy.gnybidenko@gmail.com>
2025-11-04 00:01:56 +0200

    feat: added discover and features sections to landing page

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,9 +1,13 @@
+import DiscoverSection from "./DiscoverSection";
+import FeaturesSection from "./FeaturesSection";
 import HeroSection from "./HeroSection";
 
 const Landing = () => {
 	return (
 		<div>
 			<HeroSection />
+			<FeaturesSection />
+			<DiscoverSection />
 		</div>
 	);
 };
