cef0b95cc3ce5196f1d5da7bd5df02bc247f0f5e
valeriy-developer <valeriy.gnybidenko@gmail.com>
2025-11-06 19:32:53 +0200

    feat: added a server base structure

---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+	res.send("This is home route");
+});
+
+const PORT = process.env.PORT || 3002;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
