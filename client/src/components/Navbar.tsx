6aeeea28a1243a338accbf9e43f2d03540c45ff2
valeriy-developer <valeriy.gnybidenko@gmail.com>
2025-11-18 21:11:00 +0200

    feat: added to a navbar user signin/signout information

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,14 +2,44 @@ import { NAVBAR_HEIGHT } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { useGetAuthUserQuery } from "@/state/api";
+import { usePathname, useRouter } from "next/navigation";
+import { signOut } from "aws-amplify/auth";
+import { Bell, MessageCircle, Plus, Search } from "lucide-react";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { SidebarTrigger } from "./ui/sidebar";
 
 const Navbar = () => {
+	const { data: authUser } = useGetAuthUserQuery();
+	const router = useRouter();
+	const pathname = usePathname();
+
+	const isDashboardPage =
+		pathname.includes("/managers") || pathname.includes("/tenants");
+
+	const handleSignOut = async () => {
+		await signOut();
+		window.location.href = "/";
+	};
+
 	return (
 		<div
 			className="fixed top-0 left-0 w-full z-50 shadow-xl"
 			style={{ height: `${NAVBAR_HEIGHT}px` }}>
 			<div className="flex justify-between items-center w-full py-3 px-8 bg-primary-700 text-white">
 				<div className="flex items-center gap-4 md:gap-6">
+					{isDashboardPage && (
+						<div className="md:hidden">
+							<SidebarTrigger />
+						</div>
+					)}
 					<Link href="/" className="hover:text-primary-300!" scroll={false}>
 						<div className="flex items-center gap-3">
 							<Image
@@ -27,25 +57,112 @@ const Navbar = () => {
 							</div>
 						</div>
 					</Link>
-				</div>
-				<p className="text-primary-200 hidden md:block">
-					Discover your perfect rental apartment with our advanced search
-				</p>
-				<div className="flex items-center gap-5">
-					<Link href="/signin">
-						<Button
-							variant="outline"
-							className="text-white  bg-transparent hover:bg-white hover:text-primary-700 rounded-lg">
-							Sign In
-						</Button>
-					</Link>
-					<Link href="/signup">
+					{isDashboardPage && authUser && (
 						<Button
 							variant="secondary"
-							className="text-white border-0 bg-secondary-600 hover:bg-white hover:text-primary-700 rounded-lg">
-							Sign Up
+							className="md:ml-4 bg-primary-50 text-primary-700 hover:bg-secondary-500 hover:text-primary-50"
+							onClick={() => {
+								router.push(
+									authUser.userRole?.toLowerCase() === "manager"
+										? "/managers/newproperty"
+										: "/search"
+								);
+							}}>
+							{authUser.userRole?.toLowerCase() === "manager" ? (
+								<>
+									<Plus className="w-4 h-4" />{" "}
+									<span className="hidden md:block ml-2">Add new property</span>
+								</>
+							) : (
+								<>
+									<Search className="w-4 h-4" />
+									<span className="hidden md:block ml-2">
+										Search Properties
+									</span>
+								</>
+							)}
 						</Button>
-					</Link>
+					)}
+				</div>
+				{!isDashboardPage && (
+					<p className="text-primary-200 hidden md:block">
+						Discover your perfect rental apartment with our advanced search
+					</p>
+				)}
+				<div className="flex items-center gap-5">
+					{authUser ? (
+						<>
+							<div className="relative hidden md:block">
+								<MessageCircle className="w-6 h-6 cursor-pointer text-primary-200 hover:text-primary-400" />
+								<span className="absolute top-0 right-0 w-2 h-2 bg-secondary-700 rounded-full"></span>
+							</div>
+							<div className="relative hidden md:block">
+								<Bell className="w-6 h-6 cursor-pointer text-primary-200 hover:text-primary-400" />
+								<span className="absolute top-0 right-0 w-2 h-2 bg-secondary-700 rounded-full"></span>
+							</div>
+
+							<DropdownMenu>
+								<DropdownMenuTrigger className="flex items-center gap-2 focus:outline-none">
+									<Avatar>
+										<AvatarImage src={authUser.userInfo?.image} />
+										<AvatarFallback className="bg-primary-600">
+											{authUser.userRole?.[0].toUpperCase()}
+										</AvatarFallback>
+									</Avatar>
+									<p className="text-primary-200 hidden md:block">
+										{authUser.userInfo?.name}
+									</p>
+								</DropdownMenuTrigger>
+								<DropdownMenuContent className="bg-white text-primary-700">
+									<DropdownMenuItem
+										className="cursor-pointer hover:bg-primary-700! hover:text-primary-100!"
+										onClick={() => {
+											router.push(
+												authUser.userRole?.toLowerCase() === "manager"
+													? "/managers/properties"
+													: "tenants/favorites",
+												{ scroll: false }
+											);
+										}}>
+										Go to Dashboard
+									</DropdownMenuItem>
+									<DropdownMenuSeparator className="bg-primary-200" />
+									<DropdownMenuItem
+										className="cursor-pointer hover:bg-primary-700! hover:text-primary-100!"
+										onClick={() => {
+											router.push(
+												`/${authUser.userRole?.toLowerCase()}s/settings`,
+												{ scroll: false }
+											);
+										}}>
+										Settings
+									</DropdownMenuItem>
+									<DropdownMenuItem
+										className="cursor-pointer hover:bg-primary-700! hover:text-primary-100!"
+										onClick={handleSignOut}>
+										Sign Out
+									</DropdownMenuItem>
+								</DropdownMenuContent>
+							</DropdownMenu>
+						</>
+					) : (
+						<>
+							<Link href="/signin">
+								<Button
+									variant="outline"
+									className="text-white  bg-transparent hover:bg-white hover:text-primary-700 rounded-lg">
+									Sign In
+								</Button>
+							</Link>
+							<Link href="/signup">
+								<Button
+									variant="secondary"
+									className="text-white border-0 bg-secondary-600 hover:bg-white hover:text-primary-700 rounded-lg">
+									Sign Up
+								</Button>
+							</Link>
+						</>
+					)}
 				</div>
 			</div>
 		</div>
